08b8c95d58ef0072f1429d00ff91f05f6aa63086
GandhiCloudLab <GandhiCloudLab@gmail.com>
2020-06-23 12:17:29 +0530

    dash

--- scripts/03-kube-resources-generate.sh
+++ scripts/03-kube-resources-generate.sh
@@ -7,6 +7,5 @@ ls -l ./kube-resources/
 rm -rfv ./kube-resources/*
 ls -l ./kube-resources/
 
-helm template src/gstore-purchase --output-dir ./kube-resources/
-helm template src/gstore-sales --output-dir ./kube-resources/
+helm template charts/gstoresales --output-dir ./kube-resources/
 
